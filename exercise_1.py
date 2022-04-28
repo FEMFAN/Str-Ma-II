cd78c5e9b91693a62ab4311e69ace1428aa83dbe
FEMFAN <104564465+FEMFAN@users.noreply.github.com>
2022-04-28 12:32:20 +0200

    Add files via upload

---
+++ exercise_1.py
@@ -0,0 +1,6 @@
+from CoolProp.CoolProp import PropsSI
+
+#Cp = PropsSI('C','T',288.15,'P',101325,'Nitrogen')
+
+#rho_4 = PropsSI('rho','T',800,'P',11.5e6,'water')
+h_4 = PropsSI('enthalpy','T',798,'P',11.5e6,'water')
